733de53c8e2ec03f5699eb8ad2f496aa6a417094
Lukas378 <57947679+Lukas378@users.noreply.github.com>
2019-12-17 14:18:22 +0100

    Update enemyspawner.cs

--- Assets/enemyspawner.cs
+++ Assets/enemyspawner.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class enemyspawner : MonoBehaviour
 {
+    GameObject enemyPrefab;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(Wave1());
+    }
+    IEnumerator Wave1()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(enemyPrefab, transform)
+        }
     }
 
     // Update is called once per frame

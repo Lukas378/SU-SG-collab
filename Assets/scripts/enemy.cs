59d713df50060c84b975cc1eb1cd983f40499e28
Lukas378 <57947679+Lukas378@users.noreply.github.com>
2019-12-17 15:10:39 +0100

    mager penis

---
+++ Assets/scripts/enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public int HP = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(0, -0.5f);
+        if (HP < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            HP -= 1;
+        }
+    }
+}

9e3ad7414c687b39c8a0c611b3029fd7583e6537
FredrikAlHam <43142824+FredrikAlHam@users.noreply.github.com>
2019-12-17 15:34:49 +0100

    Update InputManager.cs

--- Assets/scripts/InputManager.cs
+++ Assets/scripts/InputManager.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     public float deltaX;
+    public int hp = 100;
+    public SpriteRenderer sR;
+    public Sprite[] hSprites;
+    int spriteIndex;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sR = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -22,7 +26,12 @@ public class InputManager : MonoBehaviour
         {
             transform.position = new Vector2(transform.position.x + deltaX, transform.position.y);
         }
-        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -Screen.width/2, Screen.width/2), transform.position.y);
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -100, 100), transform.position.y);
+        if (hp > 50)
+        {
+            sR.sprite = hSprites[spriteIndex];
+            spriteIndex++;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -32,6 +41,6 @@ public class InputManager : MonoBehaviour
 
     public void Death()
     {
-
+        Debug.LogWarning("Död");
     }
 }
